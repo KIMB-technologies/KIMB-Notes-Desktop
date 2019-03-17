817fe50aa02c40b5539dcfee668ebcc6e143e60b
KIMB-technologies <info@kimb-technologies.eu>
2019-03-17 15:17:59 +0100

    Version 1.1.0

--- build.sh
+++ build.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-electron-packager ./ --platform=linux --arch=x64 --asar --icon=./assets/icons/png/128x128.png --out=./dist/
-electron-packager ./ --platform=darwin --arch=x64 --asar --icon=./assets/icons/mac/notes.icns --out=./dist/
-electron-packager ./ --platform=win32 --arch=x64 --asar --icon=./assets/icons/win/notes.ico --out=./dist/
+# Install wine to run package-win
+npm install wine-darwin
+# Setup ~/.wine by running a command
+./node_modules/.bin/wine hostnamene
+
+npm run package-win
+zip -r -9 application/KIMB-Notes-Desktop-win32-x64.zip application/KIMB-Notes-Desktop-win32-x64
+
+npm run package-linux
+zip -r -9 application/KIMB-Notes-Desktop-linux-x64.zip application/KIMB-Notes-Desktop-linux-x64
+
+npm run package-mac
+npm run create-dmg
