ec8bd523523e752b9430a4492740b66e762262a6
KIMB-technologies <info@kimb-technologies.eu>
2019-07-19 18:41:38 +0200

    New Node throws errors on build

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ npm install wine-darwin
 # Setup ~/.wine by running a command
 ./node_modules/.bin/wine hostnamene
 
+npm install electron
+
 npm run package-win
 zip -r -9 application/KIMB-Notes-Desktop-win32-x64.zip application/KIMB-Notes-Desktop-win32-x64
 
