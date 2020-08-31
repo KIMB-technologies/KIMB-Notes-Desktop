58d2f9f4116f57bf8feacd68f8c53c9db40efb19
KIMB-technologies <info@kimb-technologies.eu>
2020-08-31 19:37:22 +0200

    Fix Windows Build

--- build.sh
+++ build.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
-# Install wine to run package-win
-npm install wine-darwin
-# Setup ~/.wine by running a command
-./node_modules/.bin/wine hostnamene
-
 npm install
 
-npm run package-win
-zip -r -9 application/KIMB-Notes-Desktop-win32-x64.zip application/KIMB-Notes-Desktop-win32-x64
+if [ "$TRAVIS_OS_NAME" = "linux" ]; then
 
-npm run package-linux
-zip -r -9 application/KIMB-Notes-Desktop-linux-x64.zip application/KIMB-Notes-Desktop-linux-x64
+	# Install wine to run package-win
+	sudo apt-get install wine-stable 
+	# Setup ~/.wine by running a command
+	wine hostnamene
 
-npm run package-mac
-npm run create-dmg
+	npm run package-win
+	zip -r -9 application/KIMB-Notes-Desktop-win32-x64.zip application/KIMB-Notes-Desktop-win32-x64
+
+	npm run package-linux
+	zip -r -9 application/KIMB-Notes-Desktop-linux-x64.zip application/KIMB-Notes-Desktop-linux-x64
+
+fi;
+
+if [ "$TRAVIS_OS_NAME" = "osx" ]; then
+
+	npm run package-mac
+	npm run create-dmg
+
+fi;
